0b10482cbe51c8fc5882b0a7903fcca77719a882
evetabadmin <ap@productshare.shop>
2024-06-20 15:36:23 -0400

    removed type checks at build

--- next.config.js
+++ next.config.js
@@ -4,7 +4,20 @@ const nextConfig = {
   publicRuntimeConfig: { theme: "DEFAULT", currency: "USD" },
   images: {
     remotePatterns: [{ protocol: "https", hostname: "ui-lib.com" }]
+  },
+  reactStrictMode: true,
+  swcMinify: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: false,
   }
 };
 
-module.exports = nextConfig;
